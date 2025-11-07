daa0684c3fac3d6bf2fc3665feba38bb2013de8c
Matsura Kota <s24c1110qm@s.chibakoudai.jp>
2025-11-07 11:05:10 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 # SPDX-FileCopyrightText: 2025 Ryuichi Ueda 　　　　　
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -10,7 +10,7 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+[ "${out}" != 15 ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
